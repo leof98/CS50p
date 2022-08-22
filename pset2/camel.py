6a462058a30881b1cf11362c69872e72cfd61633
Leonardo Franco <lf290198@gmail.com>
2022-08-21 22:46:01 -0300

    Update camel.py

--- pset2/camel.py
+++ pset2/camel.py
@@ -1,11 +1,9 @@
-# implement a program that prompts the user for the name of a variable in camel case and
-# outputs the corresponding name in snake case.
+# implement a program that prompts the user for the name of a variable in camel case and outputs the corresponding name in snake case.
 # Assume that the user’s input will indeed be in camel case.
-# 20.04
 
 name = str(input("camelCase: "))
-
 snake = ""
+
 for n in name:
     if n.islower() == False:
         n = n.lower()
@@ -17,3 +15,5 @@ for n in name:
         pass
 
 print(snake)
+
+# 20.04
