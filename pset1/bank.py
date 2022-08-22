b30804cc83fd8abc54059f0066c5d39aaea6b27a
Leonardo Franco <lf290198@gmail.com>
2022-08-21 22:10:09 -0300

    Update bank.py

--- pset1/bank.py
+++ pset1/bank.py
@@ -1,9 +1,9 @@
 # Implement a program that prompts the user for a greeting.
 # If the greeting starts with “hello”, output $0.
 # If the greeting starts with an “h”, output $20.
-# Otherwise, output $100. Ignore any leading whitespace in the user’s greeting,
+# Otherwise, output $100. 
+# Ignore any leading whitespace in the user’s greeting,
 # and treat the user’s greeting case-insensitively.
-# 13.04
 
 greeting = str(input("Greeting: "))
 
@@ -13,3 +13,5 @@ elif greeting.lower().startswith('h'):
     print("$20")
 else:
     print("$100")
+    
+# 13.04
