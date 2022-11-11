4c15070f8e5e29ad02aac3a2e3f1185d08efb7db
Leonardo Franco <lf290198@gmail.com>
2022-11-11 18:25:01 -0300

    Update extensions.py

--- pset1/extensions.py
+++ pset1/extensions.py
@@ -1,28 +1,22 @@
-# implement a program that prompts the user for the name of a file and then outputs that file’s media type if the file’s name ends, case-insensitively,
-# in any of these suffixes:.gif jpg jpeg png pdf txt .zip
+"""
+Prompts the user for the name of a file and then outputs the media type if the name ends in
+(*case-insensitively) any of these suffixes: .gif .jpg .jpeg .png .pdf .txt .zip
+04.22, 11.22, 0.0
+"""
+file = str(input('file: '))
+file = file.lower().strip()
 
-file = str(input("File name: "))
-file = file.lower()
-
-if file.endswith(".gif"):
-    print("image/gif")
-
-elif file.endswith(".png"):
-    print("image/png")
-
-elif file.strip().endswith(".pdf"):
-    print("application/pdf")
-
-elif file.endswith(".txt"):
-    print("text/plain")
-
-elif file.endswith(".zip"):
-    print("application/zip")
-
-elif file.endswith(".bin"):
-    print("application/octet-stream")
-
-elif file.endswith(".jpg") or file.endswith("jpeg"):
-    print("image/jpeg")
-
-# 14.04    
+if file.endswith('.gif'):
+    print('image/gif')
+elif file.endswith('.png'):
+    print('image/png')
+elif file.endswith('.pdf'):
+    print('application/pdf')
+elif file.endswith('.txt'):
+    print('text/plain')
+elif file.endswith('.zip'):
+    print('application/zip')
+elif file.endswith('jpg') or file.endswith('jpeg'):
+    print('image/jpeg')
+else:
+    print('application/octet-stream')
