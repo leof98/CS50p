d2707312db03d76ceeb417b519212dcfe1ef29a6
Leonardo Franco <lf290198@gmail.com>
2022-11-11 18:31:03 -0300

    Update interpreter.py

--- pset1/interpreter.py
+++ pset1/interpreter.py
@@ -1,11 +1,14 @@
-# Implement a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to 
-# one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
-# x is an integer; y is +, -, *, or /; z is an integer. For instance, if the user inputs 1 + 1, your program should output 2.0
-
-exp = input("Expression: ")
-x, y, z = exp.split(" ")
+"""
+Prompts for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to one decimal place.
+Assume that the input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
+x is an integer; y is +, -, *, or /; z is an integer
+04.22, 11.22, 00.00
+"""
+exp = input('Expression: ')
+x, y, z = exp.split(' ')
 x = float(x)
-z =float(z)
+z = float(z)
+
 if y == '+':
     print(x + z)
 elif y == '-':
@@ -16,5 +19,3 @@ elif y == '/':
     print(x / z)
 else:
     print(x)
-
-# 14.04
