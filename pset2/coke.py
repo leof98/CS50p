0571e4f903348868fd98f209848d4663653012b2
Leonardo Franco <lf290198@gmail.com>
2022-11-12 15:20:37 -0300

    Update coke.py

--- pset2/coke.py
+++ pset2/coke.py
@@ -1,43 +1,21 @@
-# Implement a program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due.
-# Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
-# Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+"""
+A program that prompts the user to insert a coin, one at a time, each time informing the user of the amount due.
+Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
+04.22, 11.22, 00.00
+"""
 
 amount = 0
 change = 50
-while amount < 50:
-    n = int(input("Insert Coin: "))
+values = [5, 10, 25, 50]
 
-    if n == 5:
-        amount += 5
-        change -= 5
+while amount < 50:
+    coin = int(input('Insert Coin: '))
+    if coin in values:
+        amount += coin
+        change -= coin
         if change < 0:
-            print("Change Owed: " + str(abs(change)))
+            print('Change owed: ' + str(abs(change)))
         else:
-            print("Amount Due: " + str(change))
-            
-    elif n == 10:
-        amount += 10
-        change -= 10
-        if change < 0:
-            print("Change Owed: " + str(abs(change)))
-        else:
-            print("Amount Due: " + str(change))
-            
-    elif n == 25:
-        amount += 25
-        change -= 25
-        if change < 0:
-            print("Change Owed: " + str(abs(change)))
-        else:
-            print("Amount Due: " + str(change))
-            
-    elif n == 50:
-        amount += 50
-        change -= 50
-        if change < 0:
-            print("Change Owed: " + str(abs(change)))
-        else:
-            print("Amount Due: " + str(change))
-            
+            print(f'Amount due: {change}')
     else:
-        print("Amount Due: " + str(change))
+            print(f'Amount due: {change}')
