6726be5fec83a70ed5123584de6d3bb22338c506
Leonardo Franco <lf290198@gmail.com>
2022-11-12 19:25:27 -0300

    Update plates.py

--- pset2/plates.py
+++ pset2/plates.py
@@ -1,29 +1,36 @@
-# implement a program that prompts the user for a vanity plate and then output Valid if meets all of the
-# requirements or Invalid if it does not. Assume that any letters in the usr’s input will be uppercase.
-# Structure your program per the below, wherein is_valid returns True if s meets all requirements and
-# False if it does not. Assume that s will be a str
-# 26.04
+"""
+A program that prompts for a vanity plate and then output
+Valid if meets all of the requirements or Invalid if it does not.
+04.22, 11.22, 00.00
+"""
 
 def main():
     plate = input("Plate: ")
-
     if is_valid(plate):
         print("Valid")
     else:
         print("Invalid")
 
 def is_valid(s):
+# - Max of 6 and a min of 2
     if len(s) > 6 or len(s) < 2:
         return False
+# - Must start with at least two letters
     elif s[0].isalpha() == False or s[1].isalpha() == False:
         return False
-    elif s[2] == "0":
+# - The first number used cannot be a 0
+    elif s[2] == '0':
         return False
-    elif "." in s or "!" in s:
+# - Numbers cannot be used in the middle of a plate
+    elif s[2].isnumeric() and s[-1].isalpha():
         return False
-    elif "0" in s and s[-1].isalpha() == True:
+    elif s[3].isnumeric() and s[-1].isalpha():
         return False
-
+    elif s[-1].isnumeric() and s[-2].isalpha():
+        return False
+# - No periods, spaces, or punctuation marks are allowed
+    elif '.' in s or '!' in s:
+        return False
     else:
         return True
 
