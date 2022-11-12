de5e8ba98dd7babae71f23adc28a09efacc286fb
Leonardo Franco <lf290198@gmail.com>
2022-11-12 17:22:21 -0300

    Update twttr.py

--- pset2/twttr.py
+++ pset2/twttr.py
@@ -1,18 +1,16 @@
-# implement a program that prompts the user for a str of text and then outputs that same text but
-# with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
-# 20.04
+"""
+Implement a program that prompts the user for a str of text and then outputs that same text but
+with all vowels (A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
+04.22, 11.22, 00.00
+"""
 
-txt = str(input("Input: "))
+text = str(input('Input: '))
+txt = ''
+vogals = ['a','A','e','E','i','I','o','O', 'u', 'U']
 
-txt = txt.replace("a", "")
-txt = txt.replace("A", "")
-txt = txt.replace("e", "")
-txt = txt.replace("E", "")
-txt = txt.replace("i", "")
-txt = txt.replace("I", "")
-txt = txt.replace("o", "")
-txt = txt.replace("O", "")
-txt = txt.replace("u", "")
-txt = txt.replace("U", "")
-
-print("Output: " + txt)
+for l in text:
+    if l in vogals:
+        pass
+    else:
+        txt += l
+print(txt)
