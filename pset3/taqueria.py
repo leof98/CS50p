28a8034a95736fe9ded4999e5e92d3ec29b8a6dc
Leonardo Franco <lf290198@gmail.com>
2022-11-15 12:42:52 -0300

    Update taqueria.py

--- pset3/taqueria.py
+++ pset3/taqueria.py
@@ -1,11 +1,7 @@
-# implement a program that enables a user to place an order,
-# prompting them for items, one per line, until the user inputs
-# control-d (which is a common way of ending one’s input to a program).
-# After each inputted item, display the total cost of all items inputted thus far,
-# prefixed with a dollar sign ($) and formatted to two decimal places.
-# Treat the user’s input case insensitively.
-# Ignore any input that isn’t an item. Assume that every item on the menu will be titlecased.
-# 06.05
+"""
+Prompts for items, until ctrl-d. After each item, display the total cost of all thus far prefixed with a ($) and formatted to two decimal places.
+06.05, 11.22
+"""
 
 menu = {
     "Baja Taco": 4.00,
@@ -22,16 +18,16 @@ menu = {
 total = 0
 while True:
     try:
-        item = input("Item: ")
+        item = input('Item: ')
         item = item.title()
         menu[item]
+        
         if item in menu:
             total += menu[item]
-            print("Total: $%.2f" % (total))
+            print('Total: $%.2f' % (total))
             pass
 
     except EOFError:
         break
-
     except KeyError:
         pass
