5bc9f076a805d3c352883b47f4f8f8e576624ee4
Leonardo Franco <lf290198@gmail.com>
2022-11-17 14:33:17 -0300

    Update emojize.py

--- pset4/emojize.py
+++ pset4/emojize.py
@@ -1,9 +1,5 @@
-# Implement a program that prompts the user for a str
-# in English and then outputs the “emojized” version of that str,
-# converting any codes (or aliases) therein to their corresponding emoji.
+# Prompts for a str and outpts their corresponding emoji.
 import emoji
 
-answer = input("Input: ")
-print(emoji.emojize(answer))
-
-# 05/07
+inp = str(input('Input: '))
+print(emoji.emojize(inp, language='en'))
