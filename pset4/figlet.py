f207be6a8a347e9e5e071e188839c5b15aed2922
Leonardo Franco <lf290198@gmail.com>
2022-11-18 14:33:17 -0300

    Update figlet.py

--- pset4/figlet.py
+++ pset4/figlet.py
@@ -1,35 +1,31 @@
-# Expects zero or two command-line arguments:
-# Zero if the user would like to output text in a random font.
-# Two if the user would like to output text in a specific font, in which case the first of the two should be -f or --font, and the second of the two should be the name of the font.
-# Prompts the user for a str of text.
-# Outputs that text in the desired font.
+"""
+Prompts the user for a str of text
+Outputs that text in the desired font.
+11.22
+"""
+
 import sys
 from pyfiglet import Figlet
 import random
 
-# Verifica as condicoes, se o usuario nao fornecer nenhuma fonte, definir fonte como aleatoria
-if len(sys.argv) == 1:
-    randomFont = True
-elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font  "):
-    randomFont = False
-else:
-    sys.exit("Invalid usage")
-
 figlet = Figlet()
+font_list = figlet.getFonts()
 
-# Traz as fontes para o arquivo
-figlet.getFonts()
+if len(sys.argv) == 2:
+    sys.exit('Invalid usage')
 
-if randomFont == False:
-    try:
+if len(sys.argv) == 1:
+    inp = str(input('Input: '))
+    figlet.setFont(font=random.choice(font_list))
+    print(figlet.renderText(inp))
+
+elif len(sys.argv) == 3:
+    if sys.argv[2] not in font_list:
+        sys.exit('Invalid usage')
+        
+    if sys.argv[1] == '-f' or sys.argv[1] == '--font':
+        inp = str(input('Input: '))
         figlet.setFont(font=sys.argv[2])
-    except:
-        sys.exit("Invalid usage")
-else:
-    font = random.choice(figlet.getFonts())
-
-# Pede para o usuario um texto e imprime o texto formatado.
-text = input("Input: ")
-print("Output:")
-print(figlet.renderText(text))
-# 05/07
+        print(figlet.renderText(inp))
+    else:
+        sys.exit('Invalid usage')
