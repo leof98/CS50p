99570f08b80770659ad75bb62552076fd021a6b4
Leonardo Franco <lf290198@gmail.com>
2022-11-18 15:14:10 -0300

    Update game.py

--- pset4/game.py
+++ pset4/game.py
@@ -1,36 +1,27 @@
-# implement a program that:
-
-#Prompts the user for a level, . If the user does not input a positive integer, the program should prompt again.
-#Randomly generates an integer between 1 and , inclusive, using the random module.
-#Prompts the user to guess that integer. If the guess is not a positive integer, the program should prompt the user again.
-#If the guess is smaller than that integer, the program should output Too small! and prompt the user again.
-#If the guess is larger than that integer, the program should output Too large! and prompt the user again.
-#If the guess is the same as that integer, the program should output Just right! and exit.
-
-
+'''
+Guessing Game
+11.22
+'''
 import random
 
-# Loop until positive
 while True:
     try:
-        n = int(input("Level: "))
-        if n > 0:
+        level = int(input('Level: '))
+        if level > 0:
             break
     except:
         pass
 
 while True:
-    number = random.randint(1, n)
+    n = random.randint(1, level)
     try:
-        guess = input("Guess: ")
-        if int(guess) == number:
-            print("Just right!")
+        guess = int(input('Guess: '))
+        if guess < n:
+            print('Too small!')
+        if guess > n:
+            print('Too large!')
+        if guess == n:
+            print('Just right!')
             break
-        elif int(guess) > number:
-            print("Too large!")
-        elif int(guess) < number:
-            print("Too small!")
     except:
         pass
-
-#06/07
