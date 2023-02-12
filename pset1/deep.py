f56e5cb64408d15a776c4a34f24602370d559161
Leonardo Franco <lf290198@gmail.com>
2023-02-12 19:42:56 -0300

    Update deep.py

--- pset1/deep.py
+++ pset1/deep.py
@@ -1,7 +1,6 @@
 """
 Implement a program that prompts the user for the answer to the Great Question
 Outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two; Otherwise output No.
-04.22, 11.22, 0.0
 """
 answer = input('What is the answer to the Great Question of Life, the Universe and Everything? ')
 answer = answer.strip().lower()
